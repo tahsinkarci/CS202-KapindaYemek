5da955503a656202308a0d8967a4542da661a4ea
tahsinkarci <149708443+tahsinkarci@users.noreply.github.com>
2025-05-05 15:01:48 +0300

    Add files via upload

---
+++ KapındaYemek/code.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import mysql.connector
+import mysql.connector
+print("Connector is working!")
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="tHfB1848*D2#",
+        database="homework"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Customer")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
