046de3f8a55779254e308110e235fa822e90c92d
mardaguler <arda.guler@ozu.edu.tr>
2025-05-17 11:26:08 +0300

    DB query functions adding..

--- KapındaYemek/DB.py
+++ KapındaYemek/DB.py
@@ -7,22 +7,22 @@ class DB:
             host="localhost", user="root", password=pw, database=db, use_pure=False
         )
 
-    def getAllCustomer(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM Customer")
-        executedQuery = cursor.fetchall()
-        cursor.close()
-        self.conn.close()
-        return executedQuery   # always return query
-
-    def getUsername(self, emp_id):
+    def getUsername(self, id):  # giver user id (customer or admin)
         # get a real prepared cursor
         cur = self.conn.cursor(prepared=True)
         # NOTE: mysql.connector always uses %s placeholders
         sql = "SELECT username FROM User WHERE user_id = %s"
-        cur.execute(sql, (emp_id,))
+        cur.execute(sql, (id,))
         row = cur.fetchone()
         cur.close()
         # return the actual value instead of the SQL string
         return row
 
+    def getPassword(self, pswd):
+        cur = self.conn.cursor(prepared=True)
+        # NOTE: mysql.connector always uses %s placeholders
+        sql = "SELECT password FROM User WHERE password = %s"
+        cur.execute(sql, (pswd,))
+        row = cur.fetchone()
+        cur.close()
+        return row
